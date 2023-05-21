c502acb6a3ba0220457936a4bb4a0904b6947508
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-05-21 23:15:57 +0900

    Constant Variables

--- Dart/index.dart
+++ Dart/index.dart
@@ -1,10 +1,8 @@
 void main() {
-  late final String name;
-  // do something, go to api
-  name = "kamja"; // final 변수는 한 번만 할당할 수 있다.
-
-  late final String name2;
-  print(name2); // name2는 값을 넣기 전에 접근하지 말아야 된다고 알려준다.
-  name2 = "";
-  print(name2);
+  const API = fetchApi(); // compile-time constant가 아니다.
+  // 컴파일러가 API 변수의 값을 모르기 때문
+  // 즉, API는 const가 아닌 final이 되어야 한다.
+  final API = fetchApi();
+  const API = "121212";
+  const max_allowed_price = 120;
 }
