0d38ecb518788d421f98a5c75412893180b9aa7e
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-05-21 23:09:40 +0900

    late Variables

--- Dart/index.dart
+++ Dart/index.dart
@@ -1,8 +1,10 @@
 void main() {
-  // 한번 정의된 변수를 수정할 수 없게 만들기
-  final name = "kamja";
-  name = "kokuma"; // The final variable 'name' can only be set once.
+  late final String name;
+  // do something, go to api
+  name = "kamja"; // final 변수는 한 번만 할당할 수 있다.
 
-  // final 키워드 뒤에 타입을 정의할 수 있다.
-  final String name2 = "kokuma";
+  late final String name2;
+  print(name2); // name2는 값을 넣기 전에 접근하지 말아야 된다고 알려준다.
+  name2 = "";
+  print(name2);
 }
