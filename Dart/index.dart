8e86ca50ca115eddf747b918b1bae45302c10f79
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-05-21 18:54:44 +0900

    Dynamic Type

--- Dart/index.dart
+++ Dart/index.dart
@@ -1,4 +1,11 @@
 void main() {
-  var name = "캄자";
-  name = "kamja";
+  dynamic name;
+  if (name is String) {
+    // name이 String Type일 경우의 동작
+    // 즉, name은 String Type의 메서드를 사용할 수 있다.
+  }
+  if (name is int) {
+    // name이 Int Type일 경우의 동작
+    // 즉, name은 Int type의 메서드를 사용할 수 있다.
+  }
 }
