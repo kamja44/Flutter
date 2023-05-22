633b47881689f1bddcb0f3c75df51e5c72618d11
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-05-22 13:25:18 +0900

    Named Parameters

--- Dart/index.dart
+++ Dart/index.dart
@@ -1,18 +1,24 @@
-void sayHello(String name) {
-  print("Hello $name nice to meet you");
+// Default value
+String sayHello({
+  String name = "anon",
+  num age = 30,
+  String country = "korea",
+}) {
+  return "Hello $name, you are $age, and you come from $country";
 }
 
-String sayHello2(String name) {
-  return "Hello $name nice to meet you";
+// required 사용
+String sayHello2(
+    {required String name, required num age, required String country}) {
+  return "Hello $name, you are $age, and you come from $country";
 }
-
-String sayHello3(String name) => "Hello $name nice to meet you";
-
-num plus(num a, num b) => a + b;
 
 void main() {
-  sayHello("kamja");
-  print(sayHello2("kamja"));
-  print(sayHello3("kamja"));
-  print(plus(10, 20));
+  print(sayHello()); // name은 anon으로, age는 30으로, country는 korea로 기본값이 설정되었다.
+  // Hello anon, you are 30, and you come from korea
+  print(sayHello2(
+      age: 12,
+      country: "cicici",
+      name:
+          "lalala")); // sayHello2() argument가 전부 required이기에 인수를 전달하지 않으면 에러가 발생한다.
 }
