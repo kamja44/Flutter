4c1acf727c2445208218ab2cd51469d5a65445e0
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-05-22 23:45:04 +0900

    optional positional Parameters

--- Dart/index.dart
+++ Dart/index.dart
@@ -1,24 +1,7 @@
-// Default value
-String sayHello({
-  String name = "anon",
-  num age = 30,
-  String country = "korea",
-}) {
-  return "Hello $name, you are $age, and you come from $country";
-}
-
-// required 사용
-String sayHello2(
-    {required String name, required num age, required String country}) {
-  return "Hello $name, you are $age, and you come from $country";
-}
+String sayHello(String name, int age, [String? country = "korea"]) =>
+    "Hello $name, you are $age years old from $country";
 
 void main() {
-  print(sayHello()); // name은 anon으로, age는 30으로, country는 korea로 기본값이 설정되었다.
-  // Hello anon, you are 30, and you come from korea
-  print(sayHello2(
-      age: 12,
-      country: "cicici",
-      name:
-          "lalala")); // sayHello2() argument가 전부 required이기에 인수를 전달하지 않으면 에러가 발생한다.
+  var results = sayHello("kamja", 20);
+  print(results);
 }
