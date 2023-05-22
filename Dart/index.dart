3d10539f5ce5d1b66569ce8610651c886ed4b630
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-05-22 10:30:22 +0900

    Map

--- Dart/index.dart
+++ Dart/index.dart
@@ -1,12 +1,7 @@
 void main() {
-  var oldFriends = ["kamja", "kokuma"];
-  var newFriends = [
-    "kimchi",
-    "potato",
-    "rice",
-    for (var friend in oldFriends) "🍙  $friend",
+  List<Map<String, Object>> players = [
+    {"name": "kamja", "xp": 199993.999},
+    {"name": "kamja", "xp": 199993.999},
+    {"name": "kamja", "xp": 199993.999},
   ];
-  // list 안의 for문은 다음과 같다.
-  // for(var friend in oldFriends){newFriend.add("🍙  $friend");}
-  print(newFriends); // [kimchi, potato, rice, 🍙  kamja, 🍙  kokuma]
 }
