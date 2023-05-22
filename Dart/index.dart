54ded582848fafe8bb4ea02f8c96368685f0b8c6
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-05-22 10:36:42 +0900

    set

--- Dart/index.dart
+++ Dart/index.dart
@@ -1,7 +1,16 @@
 void main() {
-  List<Map<String, Object>> players = [
-    {"name": "kamja", "xp": 199993.999},
-    {"name": "kamja", "xp": 199993.999},
-    {"name": "kamja", "xp": 199993.999},
-  ];
+  var numbers = {1, 2, 3, 4}; // Set<int>
+  // 위 코드는 아래의 코드와 동일하다.
+  // Set<int> numbers = {1, 2, 3, 4};
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  print(numbers); // {1, 2, 3, 4}
+
+  // list 사용
+  List<int> numbers2 = [1, 2, 3, 4];
+  numbers2.add(1);
+  numbers2.add(1);
+  numbers2.add(1);
+  print(numbers2); // [1, 2, 3, 4, 1, 1, 1]
 }
