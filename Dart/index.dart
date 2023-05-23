d85035f8d0871370c7e9914211943ee16473e9f6
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-05-23 09:48:57 +0900

    constructor

--- Dart/index.dart
+++ Dart/index.dart
@@ -1,19 +1,38 @@
+// constructor 선언방법 1
 class Player {
-  String name = "kamja";
-  final int xp = 1500;
+  // late은 변수들의 값을 나중에 받아온다.
+  late String name;
+  late final int xp;
+
+  Player(String name, int xp) {
+    this.name = name;
+    this.xp = xp;
+  }
 
   void sayHello() {
     print("Hi my name is $name");
-    print("Hi my name is ${this.name}");
+  }
+}
+
+// constructor 선언방법 2
+class Player2 {
+  String name;
+  final int xp;
+
+  Player2(this.name, this.xp);
+
+  void sayHello() {
+    print("Hi my name is $name");
   }
 }
 
 void main() {
-  var player = new Player();
-  print(player.name);
-  player.name = "kokuma";
-  print(player.name);
+  var player = Player("kamja", 1500);
+  player.sayHello(); // Hi my name is kamja
 
-  // player.xp = 2000; // 'xp' can't be used as a setter because it's final.
-  player.sayHello();
+  var player2 = Player("kokuma", 3000);
+  player2.sayHello(); // Hi my name is kokuma
+
+  var player3 = Player2("kamja", 2000);
+  player3.sayHello();
 }
