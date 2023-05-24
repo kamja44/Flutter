cff9af889ac40d90d2cb5211dd614ffc2f188e8c
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-05-24 16:40:04 +0900

    Override, Super

--- Dart/index.dart
+++ Dart/index.dart
@@ -18,6 +18,12 @@ class Player extends Human {
     // Human 클래스를 상속받기에 Human 클래스의 name property를 받아야한다.
   }) : super(name: name);
   // super를 이용하여 name을 Human 클래스에 전달한다.
+
+  @override
+  void sayHello() {
+    super.sayHello(); // super키워드를 이용하면 부모 클래스의 프로퍼티에 접근하거나 메소드를 호출할 수 있다.
+    print("and I play for ${team}");
+  }
 }
 
 void main() {
