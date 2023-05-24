b981befde77e303c0254de62f14ca5dcf4ceb41b
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-05-24 13:21:56 +0900

    named Constructors

--- Dart/index.dart
+++ Dart/index.dart
@@ -1,8 +1,7 @@
 class Player {
   final String name;
-  int xp;
+  int xp, age;
   String team;
-  int age;
 
   Player({
     required this.name,
@@ -10,13 +9,35 @@ class Player {
     required this.team,
     required this.age,
   });
+
+  // Player를 초기화하는 method
+
+  // named Syntax 사용
+  // named parameter에는 기본적으로 required 속성이 없다.
+  Player.createBluePlayer({
+    required String name,
+    required int age,
+  })  : this.age = age,
+        this.name = name,
+        this.team = "blue",
+        this.xp = 0;
+
+  // positional syntax 사용
+  // 기본적으로 positional syntax는 required이다.
+  Player.createRedPlayer(String name, int age)
+      : this.age = age,
+        this.name = name,
+        this.team = "red",
+        this.xp = 0;
 }
 
 void main() {
-  var player = Player(
-    name: "kamja ",
-    xp: 1200,
-    team: "blue",
+  var bluePlayer = Player.createBluePlayer(
+    name: "kamja",
     age: 20,
+  );
+  var redPlayer = Player.createRedPlayer(
+    "kokuma",
+    30,
   );
 }
