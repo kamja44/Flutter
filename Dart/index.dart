25d47fd899b251de006f0b0e44afa6f0bbf9cc0e
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-05-24 11:27:14 +0900

    named Constructor Parameters

--- Dart/index.dart
+++ Dart/index.dart
@@ -1,38 +1,22 @@
-// constructor 선언방법 1
 class Player {
-  // late은 변수들의 값을 나중에 받아온다.
-  late String name;
-  late final int xp;
+  final String name;
+  int xp;
+  String team;
+  int age;
 
-  Player(String name, int xp) {
-    this.name = name;
-    this.xp = xp;
-  }
-
-  void sayHello() {
-    print("Hi my name is $name");
-  }
-}
-
-// constructor 선언방법 2
-class Player2 {
-  String name;
-  final int xp;
-
-  Player2(this.name, this.xp);
-
-  void sayHello() {
-    print("Hi my name is $name");
-  }
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
 }
 
 void main() {
-  var player = Player("kamja", 1500);
-  player.sayHello(); // Hi my name is kamja
-
-  var player2 = Player("kokuma", 3000);
-  player2.sayHello(); // Hi my name is kokuma
-
-  var player3 = Player2("kamja", 2000);
-  player3.sayHello();
+  var player = Player(
+    name: "kamja ",
+    xp: 1200,
+    team: "blue",
+    age: 20,
+  );
 }
